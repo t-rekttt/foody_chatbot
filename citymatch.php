fd5c7dfdb300e91e6191437c58300dbaa684f87c
T-Rekt <vietthao2000@gmail.com>
2017-02-01 22:04:06 +0700

    Upgrade citymatch and findbylocation

--- citymatch.php
+++ citymatch.php
@@ -4,7 +4,7 @@
 		$API_KEY = 'AIzaSyB0d2WX4lJsBznn8EUK52xCE0RYCYjoAYI';
 		$csv = array_map('str_getcsv', file('cities.txt'));
 		$coors = $lat.','.$long;
-		$data = _sendRequest("https://maps.googleapis.com/maps/api/geocode/json?latlng=".$coors."&key=".$API_KEY);
+		$data = _sendRequest("https://maps.googleapis.com/maps/api/geocode/json?latlng=".$coors);
 		$json = json_decode($data,1);
 		$components = $json["results"][0]['address_components'];
 		$temp=0;
@@ -24,4 +24,61 @@
 			}
 		}
 		return $city;
+	}
+
+	function city_match_new($string)
+	{
+		$API_KEY = 'AIzaSyB0d2WX4lJsBznn8EUK52xCE0RYCYjoAYI';
+		$csv = array_map('str_getcsv', file('cities.txt'));
+		if (strpos($string, "http://www.google.com/maps/place/")) {
+			$coors = explode("http://www.google.com/maps/place/", $string)[0];
+			$json = json_decode($data,1);
+			$data = _sendRequest("https://maps.googleapis.com/maps/api/geocode/json?latlng=".$coors);
+			$lat = explode(",", $coors)[0];
+			$long = explode(",", $coors)[1];
+			$status = $json["status"];
+			if ($status!=='OK') {
+				return FALSE;
+			}
+		}
+		else {
+			$data = _sendRequest("https://maps.googleapis.com/maps/api/geocode/json?address=".$string);
+			$json = json_decode($data,1);
+			$status = $json["status"];
+			if ($status!=='OK') {
+				return FALSE;
+			}
+			$lat = $json["results"][0]["geometry"]["location"]["lat"];
+			$long = $json["results"][0]["geometry"]["location"]["lng"];
+		}
+		//$coors = $lat.','.$long;
+		
+		$components = $json["results"][0]['address_components'];
+		$temp=0;
+		foreach ($components as $address) {
+			if ($address["types"][0]==="administrative_area_level_1") {
+				$temp = $address;
+			}
+		}
+		$temp_name = format($temp['long_name']);
+		$types = $temp['types'][0];
+		$status = FALSE;
+		foreach ($csv as $data) {
+			$data_name = format($data[2]);
+			if (strpos($temp_name,$data_name)!==FALSE) {
+				$city = $data[0];
+				$status = TRUE;
+				break;
+			}
+		}
+		if (!$status) {
+			return FALSE;
+		}
+		$result_arr = [
+			"status"=>TRUE,
+			"cityId"=>$city,
+			"lat"=>$lat,
+			"long"=>$long
+		];
+		return $result_arr;
 	}
--- findbylocation.php
+++ findbylocation.php
@@ -13,145 +13,297 @@ function _sendRequest($url)
   return $res->status_code === 200 ? $res->body : FALSE;
 }*/
 
-if (isset($_GET['lat']) && isset($_GET['lon']) /*&& isset($_GET['cityId'])*/) {
-  header('Content-Type: application/json');
-  $lat=$_GET['lat'];
-  $lon=$_GET['lon'];
-  $count=10;
-  $type=3;
-  $page=1;
-  $cityId= city_match($lat,$lon);
-  if ($cityId=="0") {
-    $messages = ["messages"=> 
-        [
+function find_old() {
+  if (isset($_GET['lat']) && isset($_GET['lon']) /*&& isset($_GET['cityId'])*/) {
+    header('Content-Type: application/json');
+    $lat=$_GET['lat'];
+    $lon=$_GET['lon'];
+    $count=10;
+    $type=3;
+    $page=1;
+    $cityId= city_match($lat,$lon);
+    if ($cityId=="0") {
+      $messages = ["messages"=> 
           [
-            "attachment"=>[
-              "type"=> "template",
-              "payload"=>[
-                "template_type"=>"button",
-                "text"=>"Vị trí bạn cần tìm chưa có trên hệ thống",
-                "buttons"=>[
-                  [
-                    "type"=>"show_block",
-                    "block_name"=>"menu",
-                    "title"=>"Trở về menu"
+            [
+              "attachment"=>[
+                "type"=> "template",
+                "payload"=>[
+                  "template_type"=>"button",
+                  "text"=>"Vị trí bạn cần tìm chưa có trên hệ thống",
+                  "buttons"=>[
+                    [
+                      "type"=>"show_block",
+                      "block_name"=>"menu",
+                      "title"=>"Trở về menu"
+                    ]
                   ]
                 ]
               ]
             ]
           ]
-        ]
-      ];
-    echo json_encode($messages);
-    exit();
-  }
-  if (isset($_GET['page'])) {
-    $page = $_GET['page'];
-  }
-  $query_arr = array(
-    'lat'=>$lat,
-    'lon'=>$lon,
-    'count'=>$count,
-    'type'=>$type,
-    'page'=>$page,
-    'cityId' =>$cityId
-  );
-  $query_str = http_build_query($query_arr);
-  $data = _sendRequest('https://www.foody.vn/__get/Place/HomeListPlace?'.$query_str);
-  $base_arr = 
-    [
-      "set_attributes"=>
-        [
-          "cityId"=>$cityId
-        ],
-      "messages"=> [
-        [
-          "attachment"=>[
-            "type"=>"template",
-            "payload"=>[
-              "template_type"=>"generic",
-              "elements"=>[]
-            ]
-          ]
-        ]
-      ]
-    ];
-  $json = json_decode($data,true);
-  //print_r($json);
-  //echo $json["CityId"];
-  if (count($json["Items"])===0) {
-    $messages = ["messages"=> 
-        [
+        ];
+      echo json_encode($messages);
+      exit();
+    }
+    if (isset($_GET['page'])) {
+      $page = $_GET['page'];
+    }
+    $query_arr = array(
+      'lat'=>$lat,
+      'lon'=>$lon,
+      'count'=>$count,
+      'type'=>$type,
+      'page'=>$page,
+      'cityId' =>$cityId
+    );
+    $query_str = http_build_query($query_arr);
+    $data = _sendRequest('https://www.foody.vn/__get/Place/HomeListPlace?'.$query_str);
+    $base_arr = 
+      [
+        "set_attributes"=>
+          [
+            "cityId"=>$cityId
+          ],
+        "messages"=> [
           [
             "attachment"=>[
-              "type"=> "template",
+              "type"=>"template",
               "payload"=>[
-                "template_type"=>"button",
-                "text"=>"Đã hết kết quả tìm kiếm phù hợp với yêu cầu",
-                "buttons"=>[
-                  [
-                    "type"=>"show_block",
-                    "block_name"=>"menu",
-                    "title"=>"Trở về menu"
-                  ]
-                ]
+                "template_type"=>"generic",
+                "elements"=>[]
               ]
             ]
           ]
         ]
       ];
-    echo json_encode($messages);
-    exit;
-  }
+    $json = json_decode($data,true);
+    //print_r($json);
+    //echo $json["CityId"];
+    if (count($json["Items"])===0) {
+      $messages = ["messages"=> 
+          [
+            [
+              "attachment"=>[
+                "type"=> "template",
+                "payload"=>[
+                  "template_type"=>"button",
+                  "text"=>"Đã hết kết quả tìm kiếm phù hợp với yêu cầu",
+                  "buttons"=>[
+                    [
+                      "type"=>"show_block",
+                      "block_name"=>"menu",
+                      "title"=>"Trở về menu"
+                    ]
+                  ]
+                ]
+              ]
+            ]
+          ]
+        ];
+      echo json_encode($messages);
+      exit;
+    }
 
-  foreach ($json['Items'] as $item) {
-    $Name = $item["Name"];
-    $Address = $item['Address'];
-    $AvgRatingText = $item['AvgRatingText'];
-    $PhotoUrl = $item['PhotoUrl'];
-    $url = "https://www.foody.vn".$item["Url"];
-    //echo $Address.PHP_EOL.$AvgRatingText.PHP_EOL.$PhotoUrl;
-    $base_arr["messages"][0]["attachment"]["payload"]["elements"][] = 
-      [
-        "title"=>$Name,
-        "image_url"=>$PhotoUrl,
-        "subtitle"=>"Địa chỉ: ".$Address.PHP_EOL."Đánh giá: ".$AvgRatingText,
-        "buttons"=> [[
-          "type"=>"web_url",
-          "url"=>$url,
-          "title"=>"Mở trên web"
-        ],
-        ["type"=>"element_share"]
-        ]
-      ];
-  }
+    foreach ($json['Items'] as $item) {
+      $Name = $item["Name"];
+      $Address = $item['Address'];
+      $AvgRatingText = $item['AvgRatingText'];
+      $PhotoUrl = $item['PhotoUrl'];
+      $url = "https://www.foody.vn".$item["Url"];
+      //echo $Address.PHP_EOL.$AvgRatingText.PHP_EOL.$PhotoUrl;
+      $base_arr["messages"][0]["attachment"]["payload"]["elements"][] = 
+        [
+          "title"=>$Name,
+          "image_url"=>$PhotoUrl,
+          "subtitle"=>"Địa chỉ: ".$Address.PHP_EOL."Đánh giá: ".$AvgRatingText,
+          "buttons"=> [[
+            "type"=>"web_url",
+            "url"=>$url,
+            "title"=>"Mở trên web"
+          ],
+          ["type"=>"element_share"]
+          ]
+        ];
+    }
 
-  $first_url = $json['Items'][0]["PhotoUrl"];
-  /*$base_arr["messages"][0]["attachment"]["payload"]["elements"][] = [
-      "title"=>"Xem thêm những địa điểm khác",
-      "subtitle"=>" ",
-      "image_url"=>$first_url,
-      "buttons"=>[[
-        "set_attributes" =>
-        [
-          "page"=>$page+1
-        ],
-        "title"=>"Xem thêm",
+    $first_url = $json['Items'][0]["PhotoUrl"];
+    /*$base_arr["messages"][0]["attachment"]["payload"]["elements"][] = [
+        "title"=>"Xem thêm những địa điểm khác",
+        "subtitle"=>" ",
         "image_url"=>$first_url,
-        "type"=>"show_block",
-        "block_name"=>"query"
-      ]]
-    ];*/
-  $base_arr["messages"][0]["attachment"]["payload"]["elements"][0]["buttons"][] = 
-      [
-        "set_attributes" =>
+        "buttons"=>[[
+          "set_attributes" =>
+          [
+            "page"=>$page+1
+          ],
+          "title"=>"Xem thêm",
+          "image_url"=>$first_url,
+          "type"=>"show_block",
+          "block_name"=>"query"
+        ]]
+      ];*/
+    $base_arr["messages"][0]["attachment"]["payload"]["elements"][0]["buttons"][] = 
         [
-          "page"=>$page+1
-        ],
-        "title"=>"Xem thêm địa điểm",
-        "image_url"=>$first_url,
-        "type"=>"show_block",
-        "block_name"=>"query"
-      ];
-  echo json_encode($base_arr);
+          "set_attributes" =>
+          [
+            "page"=>$page+1
+          ],
+          "title"=>"Xem thêm địa điểm",
+          "image_url"=>$first_url,
+          "type"=>"show_block",
+          "block_name"=>"query"
+        ];
+    echo json_encode($base_arr);
+  }
 }
+
+function find_new() {
+  if (isset($_GET["string"])) {
+    header('Content-Type: application/json');
+    $string=$_GET["string"];
+    $count=10;
+    $type=3;
+    $page=1;
+    $match= city_match_new($string);
+    if (!$match["status"]) {
+      $messages = ["messages"=> 
+          [
+            [
+              "attachment"=>[
+                "type"=> "template",
+                "payload"=>[
+                  "template_type"=>"button",
+                  "text"=>"Vị trí bạn cần tìm chưa có trên hệ thống",
+                  "buttons"=>[
+                    [
+                      "type"=>"show_block",
+                      "block_name"=>"menu",
+                      "title"=>"Trở về menu"
+                    ]
+                  ]
+                ]
+              ]
+            ]
+          ]
+        ];
+      echo json_encode($messages);
+      exit();
+    }
+    $lat = $match["lat"];
+    $lon = $match["long"];
+    $cityId = $match["cityId"];
+    if (isset($_GET['page'])) {
+      $page = $_GET['page'];
+    }
+    $query_arr = array(
+      'lat'=>$lat,
+      'lon'=>$lon,
+      'count'=>$count,
+      'type'=>$type,
+      'page'=>$page,
+      'cityId' =>$cityId
+    );
+    $query_str = http_build_query($query_arr);
+    $data = _sendRequest('https://www.foody.vn/__get/Place/HomeListPlace?'.$query_str);
+    $base_arr = 
+      [
+        "set_attributes"=>
+          [
+            "cityId"=>$cityId
+          ],
+        "messages"=> [
+          [
+            "attachment"=>[
+              "type"=>"template",
+              "payload"=>[
+                "template_type"=>"generic",
+                "elements"=>[]
+              ]
+            ]
+          ]
+        ]
+      ];
+    $json = json_decode($data,true);
+    //print_r($json);
+    //echo $json["CityId"];
+    if (count($json["Items"])===0) {
+      $messages = ["messages"=> 
+          [
+            [
+              "attachment"=>[
+                "type"=> "template",
+                "payload"=>[
+                  "template_type"=>"button",
+                  "text"=>"Đã hết kết quả tìm kiếm phù hợp với yêu cầu",
+                  "buttons"=>[
+                    [
+                      "type"=>"show_block",
+                      "block_name"=>"menu",
+                      "title"=>"Trở về menu"
+                    ]
+                  ]
+                ]
+              ]
+            ]
+          ]
+        ];
+      echo json_encode($messages);
+      exit;
+    }
+
+    foreach ($json['Items'] as $item) {
+      $Name = $item["Name"];
+      $Address = $item['Address'];
+      $AvgRatingText = $item['AvgRatingText'];
+      $PhotoUrl = $item['PhotoUrl'];
+      $url = "https://www.foody.vn".$item["Url"];
+      //echo $Address.PHP_EOL.$AvgRatingText.PHP_EOL.$PhotoUrl;
+      $base_arr["messages"][0]["attachment"]["payload"]["elements"][] = 
+        [
+          "title"=>$Name,
+          "image_url"=>$PhotoUrl,
+          "subtitle"=>"Địa chỉ: ".$Address.PHP_EOL."Đánh giá: ".$AvgRatingText,
+          "buttons"=> [[
+            "type"=>"web_url",
+            "url"=>$url,
+            "title"=>"Mở trên web"
+          ],
+          ["type"=>"element_share"]
+          ]
+        ];
+    }
+
+    $first_url = $json['Items'][0]["PhotoUrl"];
+    /*$base_arr["messages"][0]["attachment"]["payload"]["elements"][] = [
+        "title"=>"Xem thêm những địa điểm khác",
+        "subtitle"=>" ",
+        "image_url"=>$first_url,
+        "buttons"=>[[
+          "set_attributes" =>
+          [
+            "page"=>$page+1
+          ],
+          "title"=>"Xem thêm",
+          "image_url"=>$first_url,
+          "type"=>"show_block",
+          "block_name"=>"query"
+        ]]
+      ];*/
+    $base_arr["messages"][0]["attachment"]["payload"]["elements"][0]["buttons"][] = 
+        [
+          "set_attributes" =>
+          [
+            "page"=>$page+1
+          ],
+          "title"=>"Xem thêm địa điểm",
+          "image_url"=>$first_url,
+          "type"=>"show_block",
+          "block_name"=>"query"
+        ];
+    echo json_encode($base_arr);
+  }
+}
+
+find_old();
+find_new();
